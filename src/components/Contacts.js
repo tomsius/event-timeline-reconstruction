3fd16a8077860f40087f3e6e81fc92a37a3f2309
Tomas Kašelynas <tomaskas98@gmail.com>
2021-10-19 16:17:35 +0300

    Convert contacts component to functional

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
-export class Contacts extends Component {
-    render() {
-        return (
-            <div className="container">
-                <h1>Kontaktai</h1>
-                <div style={{ margin: "50px" }}>
-                    <p style={{textAlign: "center"}}><b>Užsakovas:</b><br />
-                        prof. Vacius Jusas.<br />
-                        El. paštas: vacius.jusas@ktu.lt</p>
-                    <p style={{textAlign: "center"}}><b>Vadovas:</b><br />
-                        prof. Vacius Jusas.<br />
-                        El. paštas: vacius.jusas@ktu.lt</p>
-                    <p style={{textAlign: "center"}}><b>Studentas:</b><br />
-                        Tomas Kašelynas<br />
-                        El. paštas: tomas.kaselynas@ktu.edu<br />
-                        Tel. numeris: 869605352</p>
-                </div>
-            </div>
-        );
+function Contacts() {
+    const { isAuthenticated } = useAuth0();
+
+    if (!isAuthenticated) {
+        return <></>
     }
+
+    return (
+        <div className="container">
+            <h1>Kontaktai</h1>
+            <div style={{ margin: "50px" }}>
+                <p style={{ textAlign: "center" }}><b>Užsakovas:</b><br />
+                    prof. Vacius Jusas.<br />
+                    El. paštas: vacius.jusas@ktu.lt</p>
+                <p style={{ textAlign: "center" }}><b>Vadovas:</b><br />
+                    prof. Vacius Jusas.<br />
+                    El. paštas: vacius.jusas@ktu.lt</p>
+                <p style={{ textAlign: "center" }}><b>Studentas:</b><br />
+                    Tomas Kašelynas<br />
+                    El. paštas: tomas.kaselynas@ktu.edu<br />
+                    Tel. numeris: 869605352</p>
+            </div>
+        </div>
+    );
 }
+
+export default Contacts;
