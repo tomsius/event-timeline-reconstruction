02d608dc6cfcc7a19dc5bcef6ed305429a4d3285
Tomas Kašelynas <tomaskas98@gmail.com>
2021-10-19 16:17:18 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Switch, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
-import {Home} from './components/Home';
-import {Contacts} from './components/Contacts';
+import { Home } from './components/Home';
+import Contacts from './components/Contacts';
+import TaskList from './components/TaskList';
 
 function App() {
   const { isLoading } = useAuth0();
 
   if (isLoading) {
-    return <div style={{textAlign: "center", position: "relative"}}>Palaukite...</div>
+    return <div style={{ textAlign: "center", position: "relative" }}>Palaukite...</div>
   }
 
   return (
@@ -17,8 +18,10 @@ function App() {
       <Navigation />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path='/semester1' exact render={(props) => (<TaskList {...props} semester={1} key={1} />)} />
+        <Route path='/semester2' exact render={(props) => (<TaskList {...props} semester={2} key={2} />)} />
+        <Route path='/semester3' exact render={(props) => (<TaskList {...props} semester={3} key={3} />)} />
         <Route exact path="/contacts" component={Contacts} />
-        {/*<Route path='/wins/bygrid' exact render={(props) => (<WinnersByGrid {...props} api={"wins/bygrid"} pageTitle={"Laimėjimai iš tam tikros starto pozicijos"} />)} />*/}
       </Switch>
     </>
   );
